60502002be87963d77681425e12db771c267ca5c
NiKravets <100012710+NiKravets@users.noreply.github.com>
2023-02-26 15:12:23 +0300

    запись и чтение с файла

---
+++ db.py
@@ -0,0 +1,36 @@
+import cNote
+
+
+def writeToFile(note, mode):
+    file = open("db.csv", mode=mode, encoding='utf-8')
+    strNote = cNote.Note.toString(note)
+    file.write(strNote)
+    file.write('\n')
+    file.close()
+
+
+def writeToList(listOfNote, mode):
+    file = open("db.csv", mode='w', encoding='utf-8')
+    file.seek(0)
+    file.close()
+    file = open("db.csv", mode='w', encoding='utf-8')
+    for note in listOfNote:
+        strNote = cNote.Note.toString(note)
+        file.write(strNote)
+        file.write('\n')
+    file.close()
+
+
+def readFromFile():
+    try:
+        file = open("db.csv", mode='r', encoding='utf-8')
+        notes = file.read().strip().split('\n')
+        listNotes = []
+        for n in notes:
+            splitNote = n.split(',')
+            note = cNote.Note(id=splitNote[0], title=splitNote[1], body=splitNote[2], date=splitNote[3])
+            listNotes.append(note)
+    except Exception:
+        print('Заметок нет')
+    finally:
+        return listNotes
