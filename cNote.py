2a9daba4d66385a445ee4bf4e6af2fd7b7ba553f
NiKravets <100012710+NiKravets@users.noreply.github.com>
2023-02-26 15:11:38 +0300

    конструктор заметки

---
+++ cNote.py
@@ -0,0 +1,36 @@
+import uuid
+from datetime import datetime
+
+
+class Note:
+    def __init__(self, id=str(uuid.uuid1())[0:2], title='текст', body='текст',
+                 date=str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))):
+        self.id = id
+        self.title = title
+        self.body = body
+        self.date = date
+
+    def getId(note):
+        return note.id
+
+    def getDate(note):
+        return note.date
+
+    def setId(note):
+        note.id = str(uuid.uuid1())[0:2]
+
+    def setTitle(note, title):
+        note.title = title
+
+    def setBody(note, body):
+        note.body = body
+
+    def setDate(note):
+        note.date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+
+    def toString(note):
+        return note.id + ',' + note.title + ',' + note.body + ',' + note.date
+
+    def forShow(note):
+        return 'id' + ': ' + note.id + '\n' + 'Заголовок' + ': ' + note.title + '\n' \
+            + 'Тело заметки' + ': ' + note.body + '\n'  + 'Дата публикации' + ': ' + note.date + "\n"
